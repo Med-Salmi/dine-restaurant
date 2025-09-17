24a94da979e1d3d84423ef6043d8d0c1adcd59ba
Mohammed Salmi <medsalmipro@gmail.com>
2025-09-17 11:52:44 +0100

    Homepage: Add cta section

---
+++ src/components/home/cta/cta.js
@@ -0,0 +1,26 @@
+import "./cta.scss";
+
+// DOM template
+function renderCtaSection() {
+  const ctaHTML = `
+    <section class="cta" aria-labelledby="cta-title">
+      <div class="cta__container max-content-width">
+        <h2 id="cta-title" class="cta__headline">
+          Ready to make a reservation?
+        </h2>
+
+        <a href="booking.html" role="button" class="cta__button btn btn--primary">
+          Book a table
+        </a>
+      </div>
+    </section>
+  `;
+
+  const main = document.querySelector("main");
+  main.insertAdjacentHTML("beforeend", ctaHTML);
+}
+
+// Initialization
+export function initCta() {
+  renderCtaSection();
+}
--- src/home.js
+++ src/home.js
@@ -2,10 +2,12 @@ import { initHero } from "./components/home/hero/hero.js";
 import { initFeatures } from "./components/home/features/features.js";
 import { initHighlights } from "./components/home/highlights/highlights.js";
 import { initEvents } from "./components/home/events/events.js";
+import { initCta } from "./components/home/cta/cta.js";
 
 export function initHome() {
   initHero();
   initFeatures();
   initHighlights();
   initEvents();
+  initCta();
 }
